7849b4e651c5fba86cf91af0d41dab40e07a131f
Saurav Swaraj <saurav.swaraj@grofers.com>
2017-11-03 14:21:07 +0530

    multiple use of just + map operator usage

--- app/src/main/java/com/swaraj/tada/Fragment/FragmentAddTasks.java
+++ app/src/main/java/com/swaraj/tada/Fragment/FragmentAddTasks.java
@@ -4,11 +4,13 @@ import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.swaraj.tada.Interfaces.LoadFragmentCallback;
 import com.swaraj.tada.R;
@@ -19,6 +21,16 @@ import com.swaraj.tada.db.TaskQueryHandler;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Function3;
 
 /**
  * Created by swaraj on 01/11/17
@@ -37,6 +49,8 @@ public class FragmentAddTasks extends Fragment {
 
     LoadFragmentCallback parentActivity;
 
+    Consumer<String > consumer;
+
     public static FragmentAddTasks getInstance() {
         return new FragmentAddTasks();
     }
@@ -49,38 +63,48 @@ public class FragmentAddTasks extends Fragment {
 
         parentActivity = (TaDaBaseActivity) getActivity();
         tasksList = new ArrayList<>();
-//        taskAdapter = new AllTaskAdapter(getContext(), tasksList);
-
 
         text = (EditText) v.findViewById(R.id.task_content);
         entryDate = (EditText) v.findViewById(R.id.task_entry_date);
         reminderDate = (EditText) v.findViewById(R.id.task_reminder_date);
         mpriority = (EditText) v.findViewById(R.id.task_priority);
-//        recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
         submit = (TextView) v.findViewById(R.id.submit);
         showTasks = (TextView) v.findViewById(R.id.show);
 
 
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-//        recyclerView.setAdapter(taskAdapter);
-//        recyclerView.setVisibility(View.GONE);
-
         asyncQueryHandler = new TaskQueryHandler(getContext().getContentResolver());
 
         setClickActions();
-
-        showTasks.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parentActivity.loadFragment(null, 1, "allTask");
-            }
-        });
         return v;
     }
 
     private void setClickActions() {
-        setActionForDbOps();
+//        setActionForDbOps();
+        setActionForRxButtons();
+        setActionForRxButtons1();
+    }
 
+    private void setActionForRxButtons() {
+
+        //to check if a class level instantiated consumer listen to observable again n again  - it listens
+        // I guess we need to unsubscribe the subscription to make it stop listening
+        consumer = getConsumer();
+
+        showTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSomething();
+            }
+        });
+    }
+
+    private void setActionForRxButtons1() {
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSomethingElse();
+            }
+        });
     }
 
     private void setActionForDbOps() {
@@ -106,11 +130,79 @@ public class FragmentAddTasks extends Fragment {
         values.put(TaskContract.AllTasks.PRIORITY, priority);
 
         getContext().getContentResolver().insert(TaskContract.AllTasks.CONTENT_URI, values);
-
-//        updateTaskList();
     }
 
-//
+    private void doSomething() {
+//        getObservable().subscribe(consumer);
+//        getObservable().subscribe(getConsumer());
+//        getObservable().subscribe(getObserver());
 
+        Observer<String> observer= getObserver();
+
+        Observable.just("Hello Android").subscribe(observer);
+        Observable.just("another parallel observable").subscribe(observer);
+    }
+
+    private void doSomethingElse() {
+        Observable.just("nikki lauda", "james hunt")
+                .map(new Function<String, String >() {
+                    @Override
+                    public String apply(@NonNull String s) throws Exception {
+                        return s + " is champ";
+                    }
+                })
+                .subscribe(getObserver());
+    }
+
+
+    private Observable<String> getObservable() {
+        Observable<String> myObservable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                int i = 4 / 0;
+                e.onNext(Integer.toString(i));
+                e.onComplete();
+            }
+        });
+
+
+        return myObservable;
+    }
+
+    private Consumer<String> getConsumer(){
+        Consumer<String> con = new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        return con;
+    }
+
+
+    private Observer<String> getObserver() {
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d("rxActions", "observer subscried");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d("rxActions", "error => " + e.toString());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("rxActions", "on complete called");
+            }
+        };
+    }
 
 }
