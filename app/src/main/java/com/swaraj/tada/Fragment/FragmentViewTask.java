5737a923ba123cdbc5c5ce02f6da6cfb5f22e088
Saurav Swaraj <saurav.swaraj@grofers.com>
2017-11-03 18:41:37 +0530

    side note for future

--- app/src/main/java/com/swaraj/tada/Fragment/FragmentViewTask.java
+++ app/src/main/java/com/swaraj/tada/Fragment/FragmentViewTask.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -144,6 +145,9 @@ public class FragmentViewTask extends Fragment {
             @Override
             public void onComplete() {
                 taskAdapter.notifyDataSetChanged();
+                Log.d("rxJava", "notify called"); // to keep count how many times onComplete is being called
+                // for the record - it was called only after all the items in Observable.fromIterable(taskses)
+                // were passed to observer
             }
         };
     }
